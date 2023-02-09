814372b6c9d03b85a483ea2275325341777ac2a3
safiyya-ona <safiyya.onanuga@hotmail.co.uk>
2023-02-09 16:31:54 +0000

    Added beam propagation with optional perlin noise psf use

--- helpers/beam_propagation.py
+++ helpers/beam_propagation.py
@@ -3,17 +3,24 @@ import torch
 import odak
 import numpy as np
 import argparse
+from perlin_noise_psf import sample_psf_as_tensor
+
+# constants for wavelength of visible light
+LOWER_WAVELENGTH = 400e-9
+UPPER_WAVELENGTH = 750e-9
+
+PI_MULT = 2 * odak.pi
 
 
 class Simulation:
-    def __init__(self, wavelengths=[532e-9], pixel_pitch=8e-6, resolution=[1080, 1920], propagation_type='Bandlimited Angular Spectrum') -> None:
+    def __init__(self, wavelengths=[532e-9], pixel_pitch=8e-6, resolution=[1080, 1920], propagation_type='Bandlimited Angular Spectrum', perlin=False) -> None:
         self.wavelengths = wavelengths
         self.pixel_pitch = pixel_pitch
         self.resolution = resolution
-        self.wavenumber = odak.wave.wavenumber(self.wavelengths[0])
         self.propagation_type = propagation_type
-        self.diffuser_phase = torch.rand(
-            resolution[0], resolution[1]) * 2 * odak.pi  # multipled by 2pi to diffuse
+        self.diffuser_phase = sample_psf_as_tensor(
+            resolution[1], resolution[0]) if perlin else torch.rand(*resolution)
+        self.diffuser_phase = self.diffuser_phase * PI_MULT
         self.diffuser_mask = odak.learn.wave.generate_complex_field(
             torch.ones_like(self.diffuser_phase), self.diffuser_phase)
 
@@ -24,7 +31,7 @@ class Simulation:
             field_before_diffuser = odak.learn.wave.propagate_beam(
                 input_field, wavenumber, distance_to_camera, self.pixel_pitch, wavelength, self.propagation_type, [True, False, True])
 
-            field_after_diffuser = field_before_diffuser * self.diffuser_mask
+            field_after_diffuser = field_before_diffuser * PI_MULT * self.diffuser_mask
             field_image_sensor = odak.learn.wave.propagate_beam(
                 field_after_diffuser, wavenumber, distance_to_sensor, self.pixel_pitch, wavelength, self.propagation_type, [True, False, True])
 
@@ -35,13 +42,15 @@ class Simulation:
         return total_image_sensor
 
 
-def diffuse_image(image_filename, num_wavelengths=1000):
+def diffuse_image(image_filename, num_wavelengths=100, distance_to_camera=0.1, distance_to_sensor=4e-3):
     # Define an input field from an image
     input_field = get_image_input_field(image_filename)
-    wavelengths = np.linspace(400e-9, 750e-9, num_wavelengths)
-    simulator = Simulation(wavelengths=wavelengths.tolist())
+    wavelengths = np.linspace(
+        LOWER_WAVELENGTH, UPPER_WAVELENGTH, num_wavelengths)
+    simulator = Simulation(wavelengths=wavelengths.tolist(),
+                           resolution=input_field.shape)
     image_sensor = simulator.simulate(
-        input_field, distance_to_camera=0.1, distance_to_sensor=4e-3)
+        input_field, distance_to_camera, distance_to_sensor)
     return image_sensor
 
 
@@ -67,9 +76,9 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    image_sensor = diffuse_image(args.image_path)
+    image_sensor = diffuse_image(args.image_path, distance_to_camera=0.1)
     odak.learn.tools.save_image(
-        'diffused_image.png', image_sensor, cmin=0, cmax=1)
+        'diffused_perlin.png', image_sensor, cmin=0, cmax=1)
 
 
 if __name__ == '__main__':
